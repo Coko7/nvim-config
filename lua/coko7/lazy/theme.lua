e521fa034bb72c09f61c0566252920a0f9a5305f
Coko <91132775+Coko7@users.noreply.github.com>
2024-05-30 13:40:26 +0200

    refactor: remove extra comments from theme.lua

--- lua/coko7/lazy/theme.lua
+++ lua/coko7/lazy/theme.lua
@@ -1,19 +1,10 @@
 return {
-    { 
+    {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
         config = function()
-            --require('rose-pine').setup({
-            --    disable_background = true,
-            --    styles = {
-            --        italic = false,
-            --    },
-            --})
-
             vim.cmd("colorscheme catppuccin-mocha")
-
-            --ColorMyPencils()
         end
     }
 }
